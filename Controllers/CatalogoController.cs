50e9c4838f7030ed7a28c7f561813c22caf52d91
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-05-06 08:31:26 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Catalogo.Models;
+
+namespace Catalogo.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private static List<Item> _items = new()
+        {
+            new Item
+            {
+                Id = 1,
+                Nombre = "Debí tirar más fotos",
+                Genero = "Urbana",
+                Ano = 2025,
+                Formato = "Digital",
+                Artista = "Bad bunny"
+            },
+            new Item {
+                Id = 2,
+                Nombre = "Harry's House",
+                Genero = "Pop",
+                Ano = 2022,
+                Formato = "CD",
+                Artista = "Harry Styles"
+            },
+            new Item {
+                Id = 3,
+                Nombre = "Unfold",
+                Genero = "K-Pop",
+                Ano = 2026,
+                Formato = "Digital",
+                Artista = "Monsta X"
+            },
+            new Item {
+                Id = 4,
+                Nombre = "Midnights",
+                Genero = "Pop",
+                Ano = 2022,
+                Formato = "CD",
+                Artista = "Taylor Swift"
+            },
+            new Item
+            {
+                Id = 5,
+                Nombre = "Guts",
+                Genero = "Pop",
+                Ano = 2023,
+                Formato = "Digital",
+                Artista = "Olivia Rodrigo"
+
+            },
+            new Item
+            {
+                Id = 6,
+                Nombre = "K de Karma",
+                Genero = "Urbana",
+                Ano = 2026,
+                Formato = "Vinilo",
+                Artista = "Kenia Os"
+        },
+            };
+
+        public IActionResult Index(string? genero)
+        {
+            var resultado = string.IsNullOrEmpty(genero) ? _items : _items.Where(i => i.Genero == genero).ToList();
+
+            ViewBag.Generos = _items.Select(i => i.Genero).Distinct().ToList();
+            ViewBag.GeneroActual = genero;
+
+            return View(resultado);
+        }
+
+        public IActionResult Detalle(int id)
+        {
+            var item = _items.FirstOrDefault(i => i.Id == id);
+            return item == null ? NotFound() : View(item);
+        }
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(Item item)
+        {
+            item.Id = _items.Count + 1;
+            _items.Add(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalogo.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+        public int Ano { get; set; }
+        public string Formato { get; set; } 
+        public string Artista { get; set; }
+
+       
+    }
+}
